2a87ed5abba767c19f9970abe7bd8e31669742d9
rmlgc <rommelgarcia4b@gmail.com>
2022-12-12 11:35:53 +0000

    gh-page fixed

--- vite.config.ts
+++ vite.config.ts
@@ -33,7 +33,18 @@ export default defineConfig({
                 target: 'https://dummy.restapiexample.com/',
                 changeOrigin: true,
                 ws: true,
-                secure: false,
+                secure: true,configure: (proxy, _options) => {
+                    proxy.on('error', (err, _req, _res) => {
+                        console.log('proxy error', err);
+                    });
+                    proxy.on('proxyReq', (proxyReq, req, _res) => {
+                        console.log('Sending Request to the Target:', req.method, req.url);
+                    });
+                    proxy.on('proxyRes', (proxyRes, req, _res) => {
+                        console.log('Received Response from the Target:', proxyRes.statusCode, req.url);
+                    });
+                },
+
             }
         },
     },
